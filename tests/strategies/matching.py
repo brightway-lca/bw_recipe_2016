ac0d81fb5d4a71c6fd09a708528728c52b4a1196
Chris Mutel <cmutel@gmail.com>
2020-06-09 10:45:22 +0200

    Fix tests for CAS normalization

--- tests/strategies/matching.py
+++ tests/strategies/matching.py
@@ -1,19 +1,7 @@
-from bw_recipe_2016.strategies.matching import match_cas_number, normalized_cas
+from bw_recipe_2016.chemidplus import canonical_cas
 
 
 def test_normalized_cas():
-    assert normalized_cas(7440473) == normalized_cas(7440473.0)
-    assert normalized_cas(7440473) == normalized_cas("7440-47-3")
-    assert normalized_cas("7440-47-3") == normalized_cas("0007440-47-3")
-
-
-# def match_cas_number(data, other):
-#     """Match based on `CAS number <https://en.wikipedia.org/wiki/CAS_Registry_Number>`. Uses the key ``CAS number`` for elements in both ``data`` and ``other``."""
-#     lookup = {normalized_cas[elem["CAS number"]]: elem.key for elem in other if "CAS number" in elem}
-#     for ds in data:
-#         for cf in unlinked(ds['exchanges']):
-#             try:
-#                 cf['input'] = lookup(cf['CAS number'])
-#             except KeyError:
-#                 pass
-#     return data
+    assert canonical_cas(7440473) == canonical_cas(7440473.0)
+    assert canonical_cas(7440473) == canonical_cas("7440-47-3")
+    assert canonical_cas("7440-47-3") == canonical_cas("0007440-47-3")
